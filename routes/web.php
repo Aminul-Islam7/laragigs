1bda493dd9c86bd63336d1da67bc1135818d735f
Aminul-Islam7 <aminulfardin7@gmail.com>
2023-06-20 15:01:20 +0600

    feat: Add functionality to delete listings

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,9 @@ Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
 // Update Listing
 Route::put('/listings/{listing}', [ListingController::class, 'update']);
 
+// Delete Listing
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
+
 
 
 // Single Listing (MUST BE AT THE LAST OF /LISTINGS)
