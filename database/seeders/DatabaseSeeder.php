00f944e89ad7c9043390a361940b0cdce6b38fb2
Aminul-Islam7 <aminulfardin7@gmail.com>
2023-06-20 22:34:35 +0600

    feat: Create relationship between Users and Listings

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\User;
 use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
@@ -22,9 +23,16 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         
-        \App\Models\User::factory(5)->create();
+        // \App\Models\User::factory(5)->create();
+
+        $user = User::factory()->create([
+            'name' => 'John Doe',
+            'email' => 'john@gmail.com'
+        ]);
         
-        Listing::factory(6)->create();
+        Listing::factory(6)->create([
+            'user_id' => $user->id
+        ]);
 
         // Listing::create([
         //     'title' => 'Laravel Senior Developer', 
